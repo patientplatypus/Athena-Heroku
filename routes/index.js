62bc9634b558bb04a3a0b9632eccf31f66393e05
patientplatypus <pweyand@gmail.com>
2017-04-29 16:32:51 -0500

    play2

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/pants', function(req.res.next){
+router.get('/pants', function(req, res, next){
   res.render('pants', { title: 'pants!'});
 });
 
