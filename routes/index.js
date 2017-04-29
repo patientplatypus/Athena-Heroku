6e648c19e5e7b92e2c58050390d048afb4627644
patientplatypus <pweyand@gmail.com>
2017-04-29 16:20:25 -0500

    playing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/pants', function(req.res.next){
+  res.render('index', { title: 'pants!'});
+})
+
 module.exports = router;
